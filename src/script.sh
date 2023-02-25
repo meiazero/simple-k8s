5ffe06a838b49921fc9a729c86441722ddf7fba4
Emanuel Avila <76269418+meiazero@users.noreply.github.com>
2023-02-24 23:25:13 -0300

    feat: interface

--- src/script.sh
+++ src/script.sh
@@ -1,3 +1,36 @@
 #! /usr/bin/sh
 
-echo "Hello World"
+echo "
+
+\tWelcome to my script
+\t
+
+-v, --version   Show version
+-h, --help      Show help
+
+
+bug report: meiazero@tutanota.com
+"
+
+# loop for read the options and execute the functions
+while true; do 
+    read -p "Enter the option: " OPTION
+    
+    case $OPTION in
+        -v|--version) 
+            echo "version option"
+            break
+            ;;
+        -h|--help) 
+            echo "help option"
+            break
+            ;;
+        exit) 
+            break
+            ;;
+        *) 
+            echo "Invalid option"
+            ;;
+    esac
+done
+
